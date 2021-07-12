9be65694483fe7a859888986dc5e52e0ed632969
shakirovGit <shakirovandreyv@yandex.ru>
2021-07-12 22:18:04 +0300

    12 задача

---
+++ src/ru.ibs.shakirov/task_12.java
@@ -0,0 +1,20 @@
+package ru.ibs.shakirov;
+
+public class task_12 {
+    public static void main(String[] args) {
+        String str = "I like Java!!!";
+
+        if (str.contains("Java")) {
+            if (str.startsWith("I like")) {
+                if (str.endsWith("!!!")) {
+                    System.out.println(str.toUpperCase());
+                }
+            }
+        }
+
+        str = str.replace("a","o");
+        System.out.println(str.substring(7,11));
+
+
+    }
+}
