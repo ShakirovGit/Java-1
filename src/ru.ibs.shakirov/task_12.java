a05b8040a28109138ba7d242c44139310aa9ff29
shakirovGit <shakirovandreyv@yandex.ru>
2021-07-15 21:39:49 +0300

    задача 12

--- src/ru.ibs.shakirov/task_12.java
+++ src/ru.ibs.shakirov/task_12.java
@@ -4,17 +4,10 @@ public class task_12 {
     public static void main(String[] args) {
         String str = "I like Java!!!";
 
-        if (str.contains("Java")) {
-            if (str.startsWith("I like")) {
-                if (str.endsWith("!!!")) {
-                    System.out.println(str.toUpperCase());
-                }
-            }
+        if (str.contains("Java") && str.startsWith("I like") && str.endsWith("!!!")) {
+            System.out.println(str.toUpperCase());
         }
-
         str = str.replace("a","o");
         System.out.println(str.substring(7,11));
-
-
     }
 }
